0156c07a2996178af0570f237b35eb51c1b54f55
超 杨 <chaoy9413@163.com>
2022-11-08 13:52:23 +0800

    add requirement.txt

--- gemini_gym/__init__.py
+++ gemini_gym/__init__.py
@@ -9,3 +9,15 @@ history:
 # import os
 # project_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../'))
 # sys.path.append(project_path)
+
+import gym
+
+from gym.envs.registration import register
+
+
+register(id="sizi-v0",
+         entry_point="modeling:SiziModeling",
+         max_episode_steps=100000,
+         reward_threshold=999)
+
+__all__ = ['gym']
